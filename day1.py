4929a9fcbadfbc98fc5086536165bbe366c33c5c
Bolledlashashank <2203a51594@sru.edu.in>
2026-07-28 13:34:54 +0530

    Tokens-Day1

---
+++ day1.py
@@ -0,0 +1,85 @@
+#print('hello everyone')
+#idel is color codeing environment
+#hastag is used as single line comment..
+#we use triple quotes(single/double)''',""" as multiple line coments
+
+'''
+Tokens --> A smallest unit in a program. There are 5 types of tokens ..
+-->Keywords
+--> identifiers
+--> literals
+--> operators
+--> punctuators
+
+#keywords -->These are reserverd words in python . we have 35 keywords...
+#help() -->keywords -->it gives list of 35 keywords -->True, false etc..
+#identifiers -->THese are the names being alloted to variable, functions , classes
+#we cannot start with uppercase / lowercase letters (a-z,A-Z) but not special characters
+
+
+
+
+
+name='shashank'
+print(name)
+email_id='shashank7@gmail.com'
+print(email_id)
+
+#variable --> named memory location where we store the data , its a place holder ..
+batch = 'cse'
+sno=23
+print(batch)
+print(sno)
+
+
+#literals --> These are constans (value(s)) -->number, character
+#operator --> operators performs specific operation -- 7 types
+
+length = 5
+breadth = 3
+area = length * breadth
+print(area)
+'''
+
+#multiassignment of variables
+
+name,location,age='codegnan','hyd',7
+print(name)
+print(location)
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
